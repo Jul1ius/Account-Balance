b0f165f695055b86e10377b0f85e8d92d2262641
its julii <nanji828@gmail.com>
2022-12-05 20:07:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Account_Balance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string vnoska = Console.ReadLine();
+            double obshtoPari = 0;
+
+            while (vnoska != "NoMoreMoney")
+            {
+                double secNum = double.Parse(vnoska);
+                if (secNum < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {secNum:f2}");
+                obshtoPari += secNum;
+                vnoska = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {obshtoPari:f2}");
+        }
+    }
+
+}
